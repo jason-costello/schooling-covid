329653e022a7e35cf57d4f86f14e050db44f7156
Jason Costello <jason.costello@rackspace.com>
2021-02-07 11:46:54 -0600

    handlers

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,33 +1,37 @@
 package server
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (s *Server) GetDistricts(w http.ResponseWriter, r *http.Request) {
-	districts, err := s.DistrictSvc.Districts()
-	if err != nil{
-		w.WriteHeader(500)
-		return
-	}
-
-	data, err := json.Marshal(districts)
-	if err != nil{
-		w.WriteHeader(500)
-		return
-	}
+	//districts, err := s.DistrictSvc.Districts()
+	//if err != nil{
+	//	w.WriteHeader(500)
+	//	return
+	//}
+	//
+	//data, err := json.Marshal(districts)
+	//if err != nil{
+	//	w.WriteHeader(500)
+	//	return
+	//}
+	//w.WriteHeader(200)
+	//w.Write(data)
 	w.WriteHeader(200)
-	w.Write(data)
-	return
+	w.Write([]byte("hi"))
 }
 
 func (s *Server) GetDistrict(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("hi"))
 
 }
 func (s *Server) GetDistrictSchools(w http.ResponseWriter, r *http.Request) {
-
+	w.WriteHeader(200)
+	w.Write([]byte("hi"))
 }
 func (s *Server) GetDistrictSchool(w http.ResponseWriter, r *http.Request) {
-
+	w.WriteHeader(200)
+	w.Write([]byte("hi"))
 }
